7d276f6c2227c729df15b9a6ea161e2a122a7c4a
acoss22 <anacseq@gmail.com>
2020-04-06 13:27:26 +0100

    routes added changes

--- server.js
+++ server.js
@@ -12,10 +12,23 @@ var usersRoute = require(`${appdir}/api/routes/users-routes.js`);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => { //Extra header options. Required in case access to this API is made from a browser.
+    res.header('Access-Control-Allow-Headers', '');
+    res.header('Access-Control-Allow-Origin', '');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+
 // init routers
 roomsRoute(app);
 usersRoute(app);
 
+app.route('*').all((req, res) => {
+    res.status(404).json({"message": "Non existant route requested. (404)"});
+});
+
+app.use(function(error, req, res, next) {console.log(JSON.stringify(req)) });
 // start server
 app.listen(port, () => {
         console.log('server lives! ');
