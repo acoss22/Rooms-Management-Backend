0d509b5c97e1264ee0f08a11dde206db1a01e771
acoss22 <anacseq@gmail.com>
2020-04-06 14:12:23 +0100

    cors

--- server.js
+++ server.js
@@ -7,26 +7,28 @@ var path = require('path');
 var appdir = path.dirname(require.main.filename);
 var roomsRoute = require(`${appdir}/api/routes/rooms-routes.js`);
 var usersRoute = require(`${appdir}/api/routes/users-routes.js`);
+const cors = require('cors');
 
+app.use(cors());
 // body parser functionality to read requests' payload
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => { //Extra header options. Required in case access to this API is made from a browser.
-    res.header('Access-Control-Allow-Headers', '');
-    res.header('Access-Control-Allow-Origin', '');
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
-    next();
-});
+// app.use((req, res, next) => { //Extra header options. Required in case access to this API is made from a browser.
+//     res.header('Access-Control-Allow-Headers', '');
+//     res.header('Access-Control-Allow-Origin', '');
+//     res.header('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+//     next();
+// });
 
 
 // init routers
 roomsRoute(app);
 usersRoute(app);
 
-app.route('*').all((req, res) => {
-    res.status(404).json({"message": "Non existant route requested. (404)"});
-});
+// app.route('*').all((req, res) => {
+//     res.status(404).json({"message": "Non existant route requested. (404)"});
+// });
 
 app.use(function(error, req, res, next) {console.log(JSON.stringify(req)) });
 // start server
